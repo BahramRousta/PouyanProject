577bd799dae7d2fcfd3db3f34dadb4bb5dfe1d37
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-14 15:07:40 +0330

    add Post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from models_utils.models import BaseModel
+from account.models import User
 
-# Create your models here.
+
+class Post(BaseModel):
+
+    content = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author.username} + {self.content}'
