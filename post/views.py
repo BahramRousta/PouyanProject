0a74a34acf1ce2afecff5d41b8b0f3d46f1e5611
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-14 15:08:50 +0330

    add create post api view

--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostAIPView(APIView):
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        """Create new post"""
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request):
+        pass
