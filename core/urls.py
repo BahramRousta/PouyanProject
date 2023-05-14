3cbe931fc3f583daf7325fd8c413b30d93ac1d36
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-14 11:57:28 +0330

    initial config

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/v1/', include('account.urls')),
+    path('comment/v1/', include('comment.urls')),
+    path('like/v1/', include('like.urls')),
+    path('post/v1/', include('post.urls')),
+]
