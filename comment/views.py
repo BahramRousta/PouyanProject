4bef0bbb24c25e45e13c24ab07ba9ef465af4c90
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-14 22:55:49 +0330

    add CommentAPIView and ReplyAPIView

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CommentPostSerializer, ReplySerializer
 
-# Create your views here.
+
+class CommentAPIView(APIView):
+    serializer_class = CommentPostSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ReplyAPIView(APIView):
+    serializer_class = ReplySerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
