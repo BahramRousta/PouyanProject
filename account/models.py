936b0cf79a638807dcfe4cdbe5459eb22acb49fb
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-14 22:53:10 +0330

    add profile model

--- account/models.py
+++ account/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
