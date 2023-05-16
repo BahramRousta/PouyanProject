3b2f08b83949cb1945b517ea03e2553bf2611865
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-16 15:48:28 +0330

    add Meta class

--- comment/models.py
+++ comment/models.py
@@ -27,5 +27,8 @@ class Reply(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created']
+
     def __str__(self):
         return f'Reply on {self.comment}'
--- post/models.py
+++ post/models.py
@@ -6,7 +6,7 @@ from account.models import Profile
 class Post(BaseModel):
     content = models.CharField(max_length=250)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    like = models.PositiveIntegerField(default=0)
+    like = models.ManyToManyField(Profile, related_name='liked_posts')
 
     def __str__(self):
         return f'Post {self.content} by {self.author.user}'
