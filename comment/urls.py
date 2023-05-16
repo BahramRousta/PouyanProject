4775084948e9ce8346383423acfd03f66752bc34
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-16 15:50:50 +0330

    add get_post_comments url

--- comment/urls.py
+++ comment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CommentAPIView, ReplyAPIView
+from .views import CommentAPIView, ReplyAPIView, GetPostComment
 
 urlpatterns = [
-    path('comment/', CommentAPIView.as_view(), name='comment'),
-    path('reply/', ReplyAPIView.as_view(), name='reply')
+    path('add_comment/', CommentAPIView.as_view(), name='add-comment'),
+    path('add_reply/', ReplyAPIView.as_view(), name='add-reply'),
+    path('get_post_comments/<int:post_id>', GetPostComment.as_view(), name='get_post_comments')
 ]
