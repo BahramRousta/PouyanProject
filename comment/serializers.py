7f4f96954deca67c120c1674338360c5ead8caab
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-16 15:49:16 +0330

    add ReplySerializer

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,25 +1,43 @@
 from rest_framework import serializers
+from account.serializers import ProfileSerializer
 from comment.models import Comment, Reply
 
 
-class CommentPostSerializer(serializers.ModelSerializer):
+class ReplySerializer(serializers.ModelSerializer):
+    author = ProfileSerializer()
+
+    class Meta:
+        model = Reply
+        fields = ('author', 'content')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    replies = ReplySerializer(many=True)
+    author = ProfileSerializer()
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'content', 'replies')
+
+
+class CommentOnPostSerializer(serializers.ModelSerializer):
     post_id = serializers.IntegerField()
 
     class Meta:
         model = Comment
-        fields = ['post_id', 'content']
+        fields = ['id', 'post_id', 'content']
 
     def create(self, validated_data):
         validated_data['author'] = self.context['request'].user.profile
         return super().create(validated_data)
 
 
-class ReplySerializer(serializers.ModelSerializer):
+class ReplyOnCommentSerializer(serializers.ModelSerializer):
     comment_id = serializers.IntegerField()
 
     class Meta:
         model = Reply
-        fields = ['comment_id', 'content']
+        fields = ['id', 'comment_id', 'content']
 
     def create(self, validated_data):
         validated_data['author'] = self.context['request'].user.profile
