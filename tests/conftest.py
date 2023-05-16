5702c5eb38cee859eaf2cf7955e3759cff1c56d8
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-16 19:10:07 +0330

    add post fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,21 @@
 import pytest
 from django.contrib.auth import get_user_model
+from post.models import Post
 
 User = get_user_model()
 
 
 @pytest.fixture
 def user():
-    return User.objects.create_user(username='testuser', password='testpass')
+    return User.objects.create_user(
+        username='testuser',
+        password='testpass'
+    )
+
+
+@pytest.fixture
+def post(user):
+    return Post.objects.create(
+        content="posttest",
+        author=user.profile
+    )
