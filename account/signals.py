a3db9e3a74093a3469ba718713fb7462ab4b6dc9
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-17 21:12:35 +0330

    add loggers initial setup

--- account/signals.py
+++ account/signals.py
@@ -1,10 +1,14 @@
+import logging
 from django.db.models.signals import post_save
 from .models import User
 from django.dispatch import receiver
 from .models import Profile
+
+logger = logging.getLogger('account')
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        logger.info("Profile created: {}".format(instance.username))
