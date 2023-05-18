22ab5340c49b1f55a6d2f43a16dcab1f885792c3
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-18 20:33:02 +0330

    separate create post and get post class

--- src/post/urls.py
+++ src/post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostAIPView, LikePostAPIView, PostsLikesAPIVIew
+from .views import GetPostAIPView, LikePostAPIView, PostsLikesAPIVIew, CreatePostAPIView
+
 urlpatterns = [
-    path('create_post/', PostAIPView.as_view(), name='create-post'),
-    path('get_posts/<str:username>', PostAIPView.as_view(), name='get-posts'),
+    path('create_post/', CreatePostAPIView.as_view(), name='create-post'),
+    path('get_posts/<str:username>', GetPostAIPView.as_view(), name='get-posts'),
     path('like_post/<int:post_id>', LikePostAPIView.as_view(), name='like-post'),
     path('users_liked_post/<int:post_id>', PostsLikesAPIVIew.as_view(), name='users-liked-post'),
 ]
